f55a4a231af36143ecd06711f78f92422ce3f557
João Paulo Uba Ferreira <joaopaulouferreira@gmail.com>
2024-02-24 20:29:38 -0300

    feat: pull to refresh and business screen

--- lib/src/features/business_profile/presentation/components/user_business_list_widget.dart
+++ lib/src/features/business_profile/presentation/components/user_business_list_widget.dart
@@ -13,40 +13,62 @@ class UserBusinessListWidget extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final myBusinessListController =
         ref.read(myBusinessListScreenControllerProvider.notifier);
-    final testProvider = ref.watch(myBusinessListScreenControllerProvider);
+    final businessListProvider =
+        ref.watch(myBusinessListScreenControllerProvider);
 
-    return FutureBuilder<List<Business?>>(
-      // b
-      future: testProvider.when(
-        data: (data) => Future.value(data),
-        loading: () => Future.delayed(const Duration(seconds: 10), () => []),
-        error: (error, stackTrace) => Future.error(error),
-      ),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return ListView.builder(
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index) {
-              final business = snapshot.data![index];
-              return ListTile(
-                title: Text(business?.name ?? ''),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () => {
-                    myBusinessListController.deleteBusiness(
-                      businessId: business?.id ?? '',
-                    ),
-                  },
-                ),
-              );
-            },
-          );
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else {
-          return const Center(child: CircularProgressIndicator());
-        }
+    return RefreshIndicator(
+      onRefresh: () async {
+        // Call your method to reload data here
+        await myBusinessListController.fetchUserBusinessList();
       },
+      child: FutureBuilder<List<Business?>>(
+        future: businessListProvider.when(
+          data: (data) => Future.value(data),
+          loading: () => myBusinessListController.getUserBusinessList(),
+          error: (error, stackTrace) => Future.error(error),
+        ),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final business = snapshot.data![index];
+                return Padding(
+                  padding:
+                      const EdgeInsets.only(top: 4.0, left: 8.0, right: 8.0),
+                  child: ListTile(
+                    title: Text(business?.name ?? ''),
+                    // onTap: () => Navigator.pushNamed(
+                    //   context,
+                    //   '/business-profile',
+                    //   arguments: business,
+                    // ),
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        color: Theme.of(context).hintColor,
+                      ),
+                    ),
+                    subtitle: Text(
+                        "${business?.email ?? ''} - ${business?.phoneNumber ?? ''}"),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () => {
+                        myBusinessListController.deleteBusiness(
+                          businessId: business?.id ?? '',
+                        ),
+                      },
+                    ),
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
     );
   }
 }
