8b44a5fe90f24c67851cb65b2946e3b1f7eb71e1
João Paulo Uba Ferreira <joaopaulouferreira@gmail.com>
2024-02-27 18:04:58 -0300

    feat: business profile screen skeleton

--- lib/src/features/my_business/presentation/screens/business_profile_screen.dart
+++ lib/src/features/my_business/presentation/screens/business_profile_screen.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:project_marba/src/features/my_business/application/business_profile_screen_controller/business_profile_screen_controller.dart';
+import 'package:project_marba/src/features/my_business/presentation/components/business_category_cards_widget.dart';
+import 'package:project_marba/src/features/user_profile/presentation/components/address_display_widget.dart';
 
 class BusinessProfileScreen extends ConsumerWidget {
   const BusinessProfileScreen({Key? key}) : super(key: key);
@@ -10,51 +13,72 @@ class BusinessProfileScreen extends ConsumerWidget {
     final business = ref.watch(businessProfileScreenControllerProvider);
     return Scaffold(
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: business != null
-              ? Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _buildProfileInfoItem('ID', business.id),
-                    _buildProfileInfoItem('Name', business.name),
-                    _buildProfileInfoItem('Email', business.email),
-                    _buildProfileInfoItem('Phone Number', business.phoneNumber),
-                    _buildProfileInfoItem(
-                        'Address', business.address.toString()),
-                    _buildProfileInfoItem('Status', business.status.toString()),
-                    _buildProfileInfoItem(
-                        'Categories', business.categories.toString()),
-                    _buildProfileInfoItem(
-                        'Offer IDs', business.offersIds.toString()),
-                  ],
-                )
-              : const Center(
-                  child: CircularProgressIndicator(),
-                ),
-        ),
+        child: business != null
+            ? Column(
+                children: [
+                  const Image(
+                      image: NetworkImage(
+                          'https://scontent.fjdf6-1.fna.fbcdn.net/v/t39.30808-6/305526771_589615162866249_4361465910529182138_n.png?_nc_cat=109&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=JttfJl4LrgwAX8RS_pf&_nc_ht=scontent.fjdf6-1.fna&oh=00_AfC2yqeXTz4zjfXxM55-D4fKh4EwyCWmBUNklqpnJgLA1w&oe=65E400ED')),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                            child: _buildProfileInfoItem(null, business.name)),
+                        Row(
+                          children: [
+                            _buildProfileInfoItem(null, business.email),
+                            Spacer(),
+                            _buildProfileInfoItem(null, business.phoneNumber),
+                          ],
+                        ),
+                        AddressDisplayWidget(address: business.address),
+                        _buildProfileInfoItem(
+                            'Status', business.status.toString()),
+                        _buildProfileInfoItem('Categorias', ''),
+                        Center(
+                          child: BusinessCategoryCards(
+                            selectedCategories: business.categories,
+                            onChanged: (value) => {},
+                          ),
+                        ),
+                        _buildProfileInfoItem(
+                            'Offer IDs', business.offersIds.toString()),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
     );
   }
 
-  Widget _buildProfileInfoItem(String label, String value) {
+  Widget _buildProfileInfoItem(String? label, String value) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            label,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 16,
-            ),
-          ),
+          label != null
+              ? Text(
+                  label,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                )
+              : const SizedBox.shrink(),
           const SizedBox(height: 4),
-          Text(
-            value,
-            style: const TextStyle(fontSize: 14),
-          ),
+          value.isEmpty
+              ? const SizedBox.shrink()
+              : Text(
+                  value,
+                  style: const TextStyle(fontSize: 16),
+                ),
         ],
       ),
     );
