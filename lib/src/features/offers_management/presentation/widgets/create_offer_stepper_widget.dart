d06d7676fc2993a6654f87a7c35448c938110a46
João Paulo Uba Ferreira <joaopaulouferreira@gmail.com>
2024-03-05 17:01:05 -0300

    fix: offer creation stepper refactor

--- lib/src/features/offers_management/presentation/widgets/create_offer_stepper_widget.dart
+++ lib/src/features/offers_management/presentation/widgets/create_offer_stepper_widget.dart
@@ -4,8 +4,12 @@ import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:project_marba/src/features/offers_management/application/offer_creation/offer_creation_controller.dart';
+import 'package:project_marba/src/features/offers_management/presentation/widgets/offer_image_selection_field.dart';
+import 'package:project_marba/src/features/offers_management/presentation/widgets/offer_type_selection_widget.dart';
 
 import '../../../../shared/models/offer/offer_model.dart';
+
+final formKey = GlobalKey<FormState>();
 
 class CreateOfferStepperWidget extends ConsumerStatefulWidget {
   const CreateOfferStepperWidget({Key? key}) : super(key: key);
@@ -20,9 +24,9 @@ class CreateOfferStepperWidgetState
   late TextEditingController _offerTypeController;
   late TextEditingController _offerTitleController;
   late TextEditingController _offerDescriptionController;
-  late TextEditingController _offerPriceController;
+  late MoneyMaskedTextController _offerPriceController;
   late TextEditingController _offerAvailableQuantityController;
-  late TextEditingController _offerItemCostController;
+  late MoneyMaskedTextController _offerItemCostController;
   late File? _offerImage;
   late OfferStatus? _offerStatus;
 
@@ -34,17 +38,19 @@ class CreateOfferStepperWidgetState
     _offerTypeController = TextEditingController();
     _offerTitleController = TextEditingController();
     _offerDescriptionController = TextEditingController();
+    _offerImage = null;
+    _offerStatus = OfferStatus.active;
+    _offerAvailableQuantityController = MaskedTextController(
+      mask: '000000000',
+    );
     _offerPriceController = MoneyMaskedTextController(
       leftSymbol: 'R\$ ',
       decimalSeparator: ',',
     );
-    _offerAvailableQuantityController = TextEditingController();
     _offerItemCostController = MoneyMaskedTextController(
       leftSymbol: 'R\$ ',
       decimalSeparator: ',',
     );
-    _offerImage = null;
-    _offerStatus = OfferStatus.active;
   }
 
   @override
@@ -62,168 +68,101 @@ class CreateOfferStepperWidgetState
   Widget build(BuildContext context) {
     final offerCreationController =
         ref.read(offerCreationControllerProvider.notifier);
-    final formKey = GlobalKey<FormState>();
+
+    Column buildOfferInfoField(
+        OfferCreationController offerCreationController) {
+      return Column(
+        children: [
+          TextFormField(
+            controller: _offerTitleController,
+            decoration: const InputDecoration(
+              labelText: 'Nome da oferta',
+            ),
+            validator: (value) => offerCreationController.validateTitle(value),
+          ),
+          TextFormField(
+            controller: _offerDescriptionController,
+            maxLines: null,
+            decoration: const InputDecoration(
+              labelText: 'Descrição',
+            ),
+            validator: (value) =>
+                offerCreationController.validateDescription(value),
+          ),
+          TextFormField(
+            controller: _offerPriceController,
+            decoration: const InputDecoration(
+              labelText: 'Preço',
+            ),
+            keyboardType: TextInputType.number,
+            validator: (value) => offerCreationController.validatePrice(value),
+          ),
+          _offerTypeController.text == 'product'
+              ? TextFormField(
+                  controller: _offerAvailableQuantityController,
+                  decoration: const InputDecoration(
+                    labelText: 'Quantidade disponível',
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: (value) =>
+                      offerCreationController.validateAvailableQuantity(value),
+                )
+              : const SizedBox.shrink(),
+          TextFormField(
+            controller: _offerItemCostController,
+            decoration: const InputDecoration(
+              labelText: 'Custo',
+            ),
+            keyboardType: TextInputType.number,
+            validator: (value) =>
+                offerCreationController.validateItemCost(value),
+          ),
+          DropdownButtonFormField<OfferStatus>(
+            value: _offerStatus,
+            decoration: const InputDecoration(
+              labelText: 'Status',
+            ),
+            items: OfferStatus.values.map((OfferStatus status) {
+              return DropdownMenuItem<OfferStatus>(
+                value: status,
+                child: Text(offerCreationController.statusTranslations[
+                        status.toString().split('.').last] ??
+                    status.toString().split('.').last),
+              );
+            }).toList(),
+            onChanged: (OfferStatus? newValue) {
+              setState(() {
+                _offerStatus = newValue;
+              });
+            },
+            validator: (OfferStatus? value) =>
+                offerCreationController.validateStatus(value.toString()),
+          ),
+        ],
+      );
+    }
 
     List<Step> steps = [
       Step(
         title: const Text('Sua oferta é um produto ou serviço?'),
-        content: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              children: [
-                Row(
-                  children: [
-                    IconButton(
-                      icon: Icon(
-                        Icons.shopping_cart_sharp,
-                        color: _offerTypeController.text == 'product'
-                            ? Colors.orange
-                            : Colors.grey,
-                        size: 50, // Change colors as needed
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          _offerTypeController.text = 'product';
-                        });
-                      },
-                    ),
-                  ],
-                ),
-                const Text('Produto'),
-              ],
-            ),
-            Column(
-              children: [
-                IconButton(
-                  icon: Icon(
-                    Icons.build_sharp,
-                    color: _offerTypeController.text == 'service'
-                        ? Colors.orange
-                        : Colors.grey,
-                    size: 50, // Change colors as needed
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _offerTypeController.text = 'service';
-                    });
-                  },
-                ),
-                const Text('Serviço'),
-              ],
-            ),
-          ],
+        content: OfferTypeSelection(
+          onTypeSelected: (value) {
+            setState(() {
+              _offerTypeController.text = value;
+            });
+          },
         ),
       ),
       Step(
         title: const Text('Dados da oferta'),
         content: Column(
           children: [
-            FormField(
-              validator: (value) => offerCreationController.validateImageUrl(
-                _offerImage?.path,
-              ),
-              builder: (FormFieldState<dynamic> field) {
-                return Column(
-                  children: [
-                    InkWell(
-                      onTap: () => offerCreationController
-                          .pickNewOfferImage()
-                          .then((value) => setState(() {
-                                _offerImage = value;
-                              })),
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height * 0.2,
-                        child: _offerImage != null
-                            ? Image.file(
-                                _offerImage!,
-                                fit: BoxFit.fill,
-                              )
-                            : const Icon(
-                                Icons.add_a_photo_sharp,
-                                size: 100,
-                              ),
-                      ),
-                    ),
-                    field.errorText != null
-                        ? Text(
-                            field.errorText.toString(),
-                            style:
-                                TextStyle(color: ThemeData().colorScheme.error),
-                          )
-                        : const SizedBox.shrink(),
-                  ],
-                );
-              },
-            ),
-            TextFormField(
-              controller: _offerTitleController,
-              decoration: const InputDecoration(
-                labelText: 'Nome da oferta',
-              ),
-              validator: (value) =>
-                  offerCreationController.validateTitle(value),
-            ),
-            TextFormField(
-              controller: _offerDescriptionController,
-              maxLines: null,
-              decoration: const InputDecoration(
-                labelText: 'Descrição',
-              ),
-              validator: (value) =>
-                  offerCreationController.validateDescription(value),
-            ),
-            TextFormField(
-              controller: _offerPriceController,
-              decoration: const InputDecoration(
-                labelText: 'Preço',
-              ),
-              keyboardType: TextInputType.number,
-              validator: (value) =>
-                  offerCreationController.validatePrice(value),
-            ),
-            _offerTypeController.text == 'product'
-                ? TextFormField(
-                    controller: _offerAvailableQuantityController,
-                    decoration: const InputDecoration(
-                      labelText: 'Quantidade disponível',
-                    ),
-                    keyboardType: TextInputType.number,
-                    validator: (value) => offerCreationController
-                        .validateAvailableQuantity(value),
-                  )
-                : const SizedBox.shrink(),
-            TextFormField(
-              controller: _offerItemCostController,
-              decoration: const InputDecoration(
-                labelText: 'Custo',
-              ),
-              keyboardType: TextInputType.number,
-              validator: (value) =>
-                  offerCreationController.validateItemCost(value),
-            ),
-            DropdownButtonFormField<OfferStatus>(
-              value: _offerStatus,
-              decoration: const InputDecoration(
-                labelText: 'Status',
-              ),
-              items: OfferStatus.values.map((OfferStatus status) {
-                return DropdownMenuItem<OfferStatus>(
-                  value: status,
-                  child: Text(offerCreationController.statusTranslations[
-                          status.toString().split('.').last] ??
-                      status.toString().split('.').last),
-                );
-              }).toList(),
-              onChanged: (OfferStatus? newValue) {
-                setState(() {
-                  _offerStatus = newValue;
-                });
-              },
-              validator: (OfferStatus? value) =>
-                  offerCreationController.validateStatus(value.toString()),
-            ),
+            OfferImageField(
+                onImageSelected: (value) => setState(() {
+                      _offerImage = value;
+                    }),
+                offerCreationController: offerCreationController),
+            buildOfferInfoField(offerCreationController),
           ],
         ),
       ),
@@ -247,34 +186,39 @@ class CreateOfferStepperWidgetState
             details.currentStep != steps.length - 1
                 ? IconButton(
                     color: Colors.orange,
-                    onPressed: details.onStepContinue,
+                    onPressed: _offerTypeController.text.isNotEmpty
+                        ? details.onStepContinue
+                        : null,
                     icon: const Icon(Icons.arrow_forward_rounded),
                   )
                 : const SizedBox.shrink(),
-            TextButton(
-              onPressed: () {
-                if (formKey.currentState!.validate()) {
-                  offerCreationController
-                      .submitOfferCreationForm(
-                        offerType: _offerTypeController.text,
-                        offerTitle: _offerTitleController.text,
-                        offerDescription: _offerDescriptionController.text,
-                        offerPrice: _offerPriceController.text,
-                        offerAvailableQuantity:
-                            _offerAvailableQuantityController.text,
-                        offerItemCost: _offerItemCostController.text,
-                        offerCategory: _offerTypeController.text,
-                        offerStatus: _offerStatus,
-                        offerImage: _offerImage,
-                      )
-                      .then((value) => Navigator.of(context).pop());
-                }
-              },
-              child: const Text(
-                'Salvar',
-                style: TextStyle(color: Colors.orange),
-              ),
-            ),
+            details.currentStep == steps.length - 1
+                ? TextButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        offerCreationController
+                            .submitOfferCreationForm(
+                              offerType: _offerTypeController.text,
+                              offerTitle: _offerTitleController.text,
+                              offerDescription:
+                                  _offerDescriptionController.text,
+                              offerPrice: _offerPriceController.text,
+                              offerAvailableQuantity:
+                                  _offerAvailableQuantityController.text,
+                              offerItemCost: _offerItemCostController.text,
+                              offerCategory: _offerTypeController.text,
+                              offerStatus: _offerStatus,
+                              offerImage: _offerImage,
+                            )
+                            .then((value) => Navigator.of(context).pop());
+                      }
+                    },
+                    child: const Text(
+                      'Salvar',
+                      style: TextStyle(color: Colors.orange),
+                    ),
+                  )
+                : const SizedBox.shrink(),
           ],
         ),
         onStepCancel: () {
@@ -282,6 +226,8 @@ class CreateOfferStepperWidgetState
             setState(() {
               _currentStep -= 1;
             });
+          } else {
+            Navigator.of(context).pop();
           }
         },
         onStepContinue: () {
---
+++ lib/src/features/offers_management/presentation/widgets/offer_image_selection_field.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:project_marba/src/features/offers_management/application/offer_creation/offer_creation_controller.dart';
+
+class OfferImageField extends StatefulWidget {
+  final Function(File?) onImageSelected;
+  final OfferCreationController offerCreationController;
+
+  const OfferImageField({
+    Key? key,
+    required this.onImageSelected,
+    required this.offerCreationController,
+  }) : super(key: key);
+
+  @override
+  State<OfferImageField> createState() => _OfferImageFieldState();
+}
+
+class _OfferImageFieldState extends State<OfferImageField> {
+  File? offerImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return FormField(
+      validator: (value) => widget.offerCreationController.validateImageUrl(
+        offerImage?.path,
+      ),
+      builder: (FormFieldState<dynamic> field) {
+        return Column(
+          children: [
+            InkWell(
+              onTap: () =>
+                  widget.offerCreationController.pickNewOfferImage().then(
+                (value) {
+                  offerImage = value;
+                  widget.onImageSelected(value);
+                },
+              ),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.2,
+                child: offerImage != null
+                    ? Image.file(
+                        offerImage!,
+                        fit: BoxFit.fill,
+                      )
+                    : const Icon(
+                        Icons.add_a_photo_sharp,
+                        size: 100,
+                      ),
+              ),
+            ),
+            field.errorText != null
+                ? Text(
+                    field.errorText.toString(),
+                    style: TextStyle(color: ThemeData().colorScheme.error),
+                  )
+                : const SizedBox.shrink(),
+          ],
+        );
+      },
+    );
+  }
+}
