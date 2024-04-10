a8927fd72a6ce00f6ac5ab5708f2feb3feb8cec2
João Paulo Uba Ferreira <joaopaulouferreira@gmail.com>
2024-04-10 14:10:05 -0300

    feat: Refactor checkout view layout and add progress indicator

--- lib/src/features/orders/presentation/views/checkout_view.dart
+++ lib/src/features/orders/presentation/views/checkout_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:project_marba/src/core/widgets/medium_vertical_space_widget.dart';
 import 'package:project_marba/src/features/location_management/presentation/widgets/address_display_widget.dart';
 import 'package:project_marba/src/features/orders/application/order_view_model/order_view_model.dart';
 
@@ -19,54 +20,69 @@ class _CheckoutViewState extends ConsumerState<CheckoutView> {
         appBar: AppBar(),
         body: Column(
           children: <Widget>[
-            const Center(child: Text('Aguardando confirmação do negócio')),
-            const Card(
-              child: Padding(
-                padding: EdgeInsets.all(30.0),
-                child: Center(
-                  child: CircularProgressIndicator(),
+            Center(
+              child: Text(
+                'Aguardando confirmação do pedido',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(80.0),
+                  child: Center(
+                    child: LinearProgressIndicator(),
+                  ),
                 ),
               ),
             ),
             Padding(
-              padding: EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(8.0),
               child: Card(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'Pedido #${order?.id.split('-').first ?? ''}',
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    Text('Total: R\$ ${order?.total.toStringAsFixed(2) ?? ''}'),
-                    AddressDisplayWidget(
-                      address: order!.address,
-                    ),
-                    const Text('Produtos'),
-                    ListView.builder(
-                      padding: const EdgeInsets.all(8.0),
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      itemCount: orderViewModel.getOrderItems()?.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          leading: Image.network(
-                            '${orderViewModel.getOrderItems()?[index].imageUrl}',
-                            width: 50,
-                            height: 80,
-                            fit: BoxFit.fill,
-                          ),
-                          title: Text(
-                              '${orderViewModel.getOrderItems()?[index].name}'),
-                          subtitle: Text(
-                              'R\$ ${orderViewModel.getOrderItems()?[index].price.toStringAsFixed(2)}'),
-                          trailing: Text(
-                              'Qtd: ${orderViewModel.getOrderItems()?[index].quantity}'),
-                        );
-                      },
-                    ),
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Pedido #${order?.id.split('-').first ?? ''}',
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      const VerticalSpaceMediumWidget(),
+                      Text(
+                          'Total: R\$ ${order?.total.toStringAsFixed(2) ?? ''}'),
+                      const VerticalSpaceMediumWidget(),
+                      AddressDisplayWidget(
+                        address: order!.address,
+                      ),
+                      const VerticalSpaceMediumWidget(),
+                      const Text('Produtos'),
+                      ListView.builder(
+                        padding: const EdgeInsets.all(2.0),
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        itemCount: orderViewModel.getOrderItems()?.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading: Image.network(
+                              '${orderViewModel.getOrderItems()?[index].imageUrl}',
+                              width: 50,
+                              height: 80,
+                              fit: BoxFit.fill,
+                            ),
+                            title: Text(
+                                '${orderViewModel.getOrderItems()?[index].name}'),
+                            subtitle: Text(
+                                'R\$ ${orderViewModel.getOrderItems()?[index].price.toStringAsFixed(2)}'),
+                            trailing: Text(
+                                'Qtd: ${orderViewModel.getOrderItems()?[index].quantity}'),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
