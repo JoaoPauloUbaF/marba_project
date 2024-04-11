da9b6456de39c2bdafc3a6d4b6a52fe514b392ab
João Paulo Uba Ferreira <joaopaulouferreira@gmail.com>
2024-04-11 17:16:35 -0300

    orders repositories impl

---
+++ lib/src/features/orders/data/user_orders_repository/user_orders_repository_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_orders_repository_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userOrdersRepositoryHash() =>
+    r'3a4ada454094a10cec0e5d533786fa22b4297858';
+
+/// See also [userOrdersRepository].
+@ProviderFor(userOrdersRepository)
+final userOrdersRepositoryProvider =
+    AutoDisposeProvider<UserOrdersRepository>.internal(
+  userOrdersRepository,
+  name: r'userOrdersRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$userOrdersRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef UserOrdersRepositoryRef = AutoDisposeProviderRef<UserOrdersRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
